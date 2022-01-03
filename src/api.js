300093b5df6e5f913b6b0b1f82adb1530097961d
DesignC <znehraks@gmail.com>
2022-01-04 01:02:31 +0900

    deploy ready

--- src/api.js
+++ src/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 const api = axios.create({
-  // baseURL: "https://capstone.run.goorm.io/",
-  baseURL: "http://localhost:4000/",
-  // baseURL: "https://resi-reco-backend.herokuapp.com/",
+  // baseURL: "http://localhost:4000/",
+  baseURL: "https://unibangcity-backend.herokuapp.com/",
 });
 
 //api 모아두기
